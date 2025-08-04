615ee6c48e52aaf651c9c23b2f395dd1999f31eb
Jailton Santos <santosjailton98@gmail.com>
2025-08-04 12:58:40 +0000

    O erro ainda persiste veja o que aparece no console do navegador:
    page.

--- src/app/public/student/[id]/portal/page.tsx
+++ src/app/public/student/[id]/portal/page.tsx
@@ -1,16 +1,14 @@
 
-import StudentPublicPortal from "@/components/students/student-public-portal";
-import { Suspense } from "react";
-import { Skeleton } from "@/components/ui/skeleton";
 import PublicHeader from "@/components/layout/public-header";
+import StudentPublicPortal from "@/components/students/student-public-portal";
 
-export default function StudentPortalPage({ params }: { params: { id: string } }) {
-    return (
-        <>
-            <PublicHeader studentId={params.id} />
-            <Suspense fallback={<div className="flex flex-col min-h-screen bg-muted p-4 sm:p-8"><Skeleton className="max-w-4xl mx-auto w-full h-96" /></div>}>
-                <StudentPublicPortal studentId={params.id} />
-            </Suspense>
-        </>
-    );
+export default async function StudentPortalPage({ params }: { params: { id: string } }) {
+  const studentId = params.id;
+  
+  return (
+    <>
+      <PublicHeader studentId={studentId} />
+      <StudentPublicPortal studentId={studentId} />
+    </>
+  );
 }
