1ac112f270908b70da22924bab8712253fff826c
Eduardo Rodrigues <edusacramento9@gmail.com>
2025-08-18 20:34:44 -0300

    feat: Adiciona action de sign in

---
+++ src/actions/sign-in/index.ts
@@ -0,0 +1,22 @@
+"use server";
+
+import { createClient } from "@/lib/supabase/server";
+
+interface SignIn {
+  email: string;
+  password: string;
+}
+
+export const signInAction = async ({ email, password }: SignIn) => {
+  const supabase = createClient();
+  const { data: authData, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error || !authData.user) {
+    throw new Error(error?.message || "Credenciais inválidas");
+  }
+
+  return authData
+}
